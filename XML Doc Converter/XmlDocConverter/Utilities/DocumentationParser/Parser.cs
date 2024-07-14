687e2c6534c2fd24140e97988d323273f04179e9
TigrisDK <45198976+kamar-dk@users.noreply.github.com>
2024-07-14 14:27:13 +0200

    Create Parser.cs

---
+++ XML Doc Converter/XmlDocConverter/Utilities/DocumentationParser/Parser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace XmlDocConverter.Utilities.DocumentationParser
+{
+    public class Parser
+    {
+        public static string GenerateAnchor(string memberName)
+        {
+            // Convert to lowercase, remove invalid characters, and remove spaces
+            return Regex.Replace(memberName.ToLower(), @"[^\w]+", "").Trim();
+        }
+
+        /// <summary>
+        /// Method for cleaning whitespace from a string
+        /// </summary>
+        /// <param name="input">String that needs to be cleaned</param>
+        /// <returns>Returns a cleaned string</returns>
+        public static string CleanWhitespace(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            return Regex.Replace(input, @"\s+", " ").Trim();
+        }
+    }
+}
