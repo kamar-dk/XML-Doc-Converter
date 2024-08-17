fd1754da8fccc25d237a7c7cc8656487344b2658
TigrisDK <45198976+kamar-dk@users.noreply.github.com>
2024-08-17 16:31:23 +0200

    Update Program.cs

--- XmlDocConverterConsole/Program.cs
+++ XmlDocConverterConsole/Program.cs
@@ -13,34 +13,6 @@ namespace XmlDocConverterConsole
             System.Threading.Thread.Sleep(2000);
             Controller controller = new Controller();
             controller.Menu();
-            
-
-            /*if (args.Length < 2)
-            {
-                Console.WriteLine("Usage: XmlDocConverterConsole <inputFilePath> <outputFilePath>");
-                return;
-            }
-
-            string inputFilePath = args[0];
-            string outputFilePath = args[1];
-
-            if (!File.Exists(inputFilePath))
-            {
-                Console.WriteLine($"Input file not found: {inputFilePath}");
-                return;
-            }
-
-            try
-            {
-                ConvertXmlDocumentation(inputFilePath, outputFilePath);
-                Console.WriteLine($"Conversion successful! Output file created at: {outputFilePath}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An error occurred: {ex.Message}");
-            }*/
         }
-
-        
     }
 }
